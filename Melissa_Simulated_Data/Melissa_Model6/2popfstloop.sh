d56dd148a2bb494a6c71e467cb30ed58d58e1d4a
Michael Kuzminskiy <michaelkuzminskiy@gmail.com>
2023-01-10 20:24:17 -0800

    updating with fst directories

---
+++ Melissa_Simulated_Data/Melissa_Model6/2popfstloop.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+for FILE in *.vcf;
+do echo $FILE;
+vcftools --vcf $FILE --weir-fst-pop 'pop1.txt' --weir-fst-pop 'pop2.txt' --fst-window-size 100 --fst-window-step 100 --out $FILE.2pop.fst
+done
