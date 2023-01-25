0214f5bd3ee7bfdb02cd5d60d85bcf0fd684a235
Michael Kuzminskiy <michaelkuzminskiy@gmail.com>
2023-01-25 15:23:48 -0800

    used model_renamer to rename Recent_Divergence_Simulated_Data files to models 1-5 to match melissa models

---
+++ Recent_Divergence_Simulated_Data/Model4/model_renamer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Loop through 10 subdirectories
+for i in $(seq 1 10); do
+  #Change into the i-th subdirectory
+  cd ./replicate$i
+
+  #Find all files with "model4" in their name
+  files=$(find . -name "*model4*")
+
+  #Loop through all matching files
+  for file in $files; do
+    #Rename the file from model4 to model2
+    mv "$file" "${file/model10/model4}"
+  done
+
+  #Change back to the parent directory
+  cd ..
+done
