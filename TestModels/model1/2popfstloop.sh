dacc9a36bd4822df9d1e8d7c3befe56ec60c84a1
ClassicalKooz <mkuzminskiy8676@sdsu.edu>
2023-03-13 00:58:44 -0700

    cleaning up main directory

--- TestModels/model1/2popfstloop.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for FILE in *.vcf;
-do echo $FILE;
-vcftools --vcf $FILE --weir-fst-pop 'pop1.txt' --weir-fst-pop 'pop2.txt' --fst-window-size 100 --fst-window-step 100 --out $FILE.2pop.fst
-done
